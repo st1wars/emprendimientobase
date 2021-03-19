7a664e3f4e3ed7ce35c9c0f47ce63c2fdbd58a92
st1wars <80965515+st1wars@users.noreply.github.com>
2021-03-19 08:45:07 -0500

    Add files via upload

---
+++ formulario/js/funciones.js
@@ -0,0 +1,26 @@
+jQuery(document).ready(listo);
+
+
+
+function listo()
+{
+	jQuery(".hamb").click(function(e){	
+		e.preventDefault();
+		jQuery("header .container nav").toggleClass("open");
+		jQuery(".hamb i").toggleClass("fa-times");
+	
+	});
+	jQuery("header .container nav a").click(function(){
+
+		jQuery("header .container nav").toggleClass("open");
+		jQuery(".hamb i").removeClass("fa-times");
+
+		var dev = jQuery(this).attr("href");
+
+		jQuery("html,body").animate({
+			"srcrollTop": jQuery(dev).offset().top - 76
+		})
+	})
+
+}
+
